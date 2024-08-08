502d5bdc6151c00521c34abae5c36920b690553d
Sahil_JR <sahiljr1093@gmail.com>
2024-08-08 21:01:30 +0530

    add task popup ui changing

--- src/Components/Pages/TaskCreation/Modal/TaskCreateMoodal.jsx
+++ src/Components/Pages/TaskCreation/Modal/TaskCreateMoodal.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosCloseCircleOutline } from "react-icons/io";
-import { Input, Modal, TagPicker, Uploader } from "rsuite";
+import { Button, DatePicker, Input, Modal, Panel, SelectPicker, Uploader } from "rsuite";
 import CustomTagPicker from "./CustomTagPicker";
 import DateInput from "../../../Common/UI/DatePicker/DateInput";
+import { TbFlag3Filled } from "react-icons/tb";
+import { IoLinkSharp } from "react-icons/io5";
+import { FaBell } from "react-icons/fa";
+import { LuSendHorizonal } from "react-icons/lu";
 
 const TaskCreateMoodal = ({ open, setOpen }) => {
   const handleClose = () => setOpen(false);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [description, setDescription] = useState(false);
+  const [comments, setComments] = useState(false);
+  const handleUpload = (fileList) => {
+    const filesArray = Array.from(fileList).map(file => ({
+      name: file.name,
+      url: URL.createObjectURL(file.blobFile)
+    }));
+    setUploadedFiles(filesArray);
+  };
+
+  const handleRemove = (index) => {
+    const newFiles = uploadedFiles.filter((_, i) => i !== index);
+    setUploadedFiles(newFiles);
+  };
+
+  const priority = [
+    {
+      label: "Normal",
+      value: "Normal",
+    },
+    {
+      label: "Urgent",
+      value: "Urgent",
+    },
+    {
+      label: "High",
+      value: "High",
+    },
+    {
+      label: "Low",
+      value: "Low",
+    },
+  ]
   return (
     <>
       <Modal
@@ -39,12 +77,30 @@ const TaskCreateMoodal = ({ open, setOpen }) => {
               />
             </div>
             <div className="form-group">
-              <span className=" border-bottom border-dark fw-bolder">
+              <span className=" border-bottom border-dark fw-bolder" onClick={() => setDescription(true)}>
                 + Add Description
               </span>
+              {description && (
+                <Input
+                  as="textarea"
+                  placeholder="lorem impsum dolor sit amet"
+                  id="description"
+                  className="form-control border my-2"
+                  name="firstName"
+                  rows="4"
+                />
+              )}
             </div>
-            <div>
-              <DateInput placeholder="Due Date"  />
+            <div className="d-flex justify-content-between">
+              <div className="gap-3 d-flex">
+                <DatePicker placeholder="Due Date" />
+                <SelectPicker data={priority} searchable={false} placeholder="Priority" />
+                <Button className="w-50"> Time to do </Button>
+              </div>
+              <div className="d-flex">
+                <IoLinkSharp />
+                <FaBell />
+              </div>
             </div>
             <div className="form-group">
               <label className="form-label">Assign</label>
@@ -58,6 +114,7 @@ const TaskCreateMoodal = ({ open, setOpen }) => {
                   fileListVisible={false}
                   draggable
                   autoUpload={false}
+                  onChange={handleUpload}
                 >
                   <div>
                     <span className="file-icon"></span>
@@ -67,12 +124,73 @@ const TaskCreateMoodal = ({ open, setOpen }) => {
                     </p>
                   </div>
                 </Uploader>
+                {uploadedFiles.length > 0 && (
+                  <Panel header="Uploaded Files">
+                    {uploadedFiles.map((file, index) => (
+                      <div key={index} className="d-flex align-items-start p-2 justify-content-between mb-4 border rounded-3">
+                        {/* <Icon icon="file-o" style={{ marginRight: 10 }} /> */}
+                        <div className="d-flex align-items-center gap-2">
+                          <img src={file.url} alt={file.name} className="rounded-2" style={{ maxWidth: '50px', height: '50px' }} />
+                          <p>{file.name}</p>
+                        </div>
+                        <Button onClick={() => handleRemove(index)} appearance="link" color="red">
+                          <IoIosCloseCircleOutline className="d-block" size={24} />
+                        </Button>
+                      </div>
+                    ))}
+                  </Panel>
+                )}
               </div>
             </div>
             <div className="form-group">
-              <span className=" border-bottom border-dark fw-bolder">
-                + Add comments
-              </span>
+              {!comments ? (
+                <span className=" border-bottom border-dark fw-bolder" onClick={() => setComments(true)}>
+                  + Add comments
+                </span>
+              ) : (
+                <React.Fragment>
+                  <h6>Comments</h6>
+                  <div className="comment-wrapper my-2">
+                    <div className="d-flex align-items-center gap-2">
+                      <img src="" className="rounded-circle border" alt="*prifile" height={20} width={20} />
+                      <span className="fw-bold">Tyler Hunt</span>
+                    </div>
+                    <Input
+                      as="input"
+                      placeholder="lorem impsum dolor sit amet"
+                      id="description"
+                      disabled
+                      className="form-control border my-2 mx-4"
+                      name="firstName"
+                      rows="4" />
+                    <div className="d-flex align-items-center gap-2">
+                      <img src="" className="rounded-circle border" alt="*prifile" height={20} width={20} />
+                      <span className="fw-bold">Austin Henroid</span>
+                    </div>
+                    <Input
+                      as="input"
+                      placeholder="lorem impsum dolor sit amet"
+                      id="description"
+                      disabled
+                      className="form-control border my-2 ms-4"
+                      name="firstName"
+                      rows="4" />
+                  </div>
+                  <div className="comment-writer d-flex align-items-center gap-2" >
+                    <Input
+                      as="input"
+                      placeholder="Type commnet"
+                      id="description"
+                      className="form-control border my-2 w-100"
+                      name="firstName"
+                      rows="4"
+                    />
+                    <Button className="button-fill" style={{ width: '50px', height: '40px' }}>
+                      <LuSendHorizonal color="white" /> send
+                    </Button>
+                  </div>
+                </React.Fragment>
+              )}
             </div>
             <div className="d-flex align-items-center gap-3">
               <button className="button-outline" onClick={handleClose}>
